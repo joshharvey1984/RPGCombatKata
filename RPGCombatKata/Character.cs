8281738c51ad6cec81b491eac41a98b177b2ffb4
Josh <joshharvey@btopenworld.com>
2020-09-19 12:39:18 +0100

    Set up

---
+++ RPGCombatKata/Character.cs
@@ -0,0 +1,5 @@
+﻿namespace RPGCombatKata {
+    public class Character {
+        
+    }
+}
---
+++ RPGCombatKata/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace RPGCombatKata {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
