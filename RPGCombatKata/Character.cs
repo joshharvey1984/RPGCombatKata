013bb40cadf25493ce91393d9f368350cf8b1efd
Josh <joshharvey@btopenworld.com>
2020-09-19 12:56:32 +0100

    Set up

--- RPGCombatKata/Character.cs
+++ RPGCombatKata/Character.cs
@@ -1,5 +1,23 @@
 ﻿namespace RPGCombatKata {
     public class Character {
-        
+        private int _health = 1000;
+        private int Health {
+            get => _health;
+            set {
+                _health = value;
+                if (_health < 0) _health = 0;
+                IsAlive = _health > 0;
+            }
+        }
+        public int Level { get; set; } = 1;
+        private bool IsAlive { get; set; } = true;
+
+        public void DamageOtherCharacter(Character character) {
+            character.Health -= 100;
+        }
+
+        public void Heal(int amount) {
+            Health += amount;
+        }
     }
 }
